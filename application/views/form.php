0a56b9c1190568c986ae3b878b9c1a403f77a696
Izzanabilarosananda <nabilarsnnd@gmail.com>
2018-04-25 16:08:21 +0700

    first commit

---
+++ application/views/form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+	Nama <br>
+	<input type="text" name="username"><br>
+	Password <br>
+	<input type="password" name="password"><br>
+	Level
+	<select>
+		<option value="admin">Admin</option>
+		<option value="User">User</option>
+	</select>
+	<input type="submit" name="simpan" value="save">
+</form>
+</body>
+</html>
